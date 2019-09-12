d2c161b3d4a2d9fdb6891ce2a79089141ad4fd87
John Kennedy Akabo <developer.akabojohnkennedy@gmail.com>
2019-09-12 19:10:33 +0000

    Learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees"  @delete:employee="deleteEmployee" @update:employee="updateEmployee"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeForm from "@/components/EmployeeForm";
+import EmployeeTable from "@/components/EmployeeTable.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    EmployeeForm,
+    EmployeeTable
+  },
+  data: () => {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const newEmployee = { ...employee, id: lastId + 1 };
+      this.employees = [...this.employees, newEmployee];
+    },
+    deleteEmployee(id) {
+      this.employees = this.employees.filter(
+        employee => employee.id !== id
+      )
+    },
+    updateEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map(employee => employee.id = id ? updatedEmployee : employee)
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="employee-form">
+        <form @submit.prevent="handleSubmit">
+            <label>Employee Name</label>
+            <input type="text" v-model="employee.name" ref="first" />
+            <label>Employee Email</label>
+            <input type="email" v-model="employee.email" />
+            <button type="submit">Add employee</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'employee-form',
+    data: () => {
+        return {
+            employee: {
+                name: '',
+                email: ''
+            }
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit('add:employee', this.employee);
+            this.$refs.first.focus();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
